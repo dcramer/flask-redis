8675bf8b505664d6ab51fc45f3756ef842f118eb
MaximBublis <b@codemonkey.ru>
2011-02-02 16:14:33 +0300

    Extension implemented

---
+++ flaskext/redis.py
@@ -0,0 +1,35 @@
+"""
+"""
+
+from __future__ import absolute_import
+import redis
+from flask import g
+
+class Redis(object):
+
+    def __init__(self, app=None):
+
+        if app is not None:
+            self.init_app(app)
+        else:
+            self.app = None
+
+    def init_app(self, app):
+        """
+        Used to initialize redis with app object
+        """
+        app.config.setdefault('REDIS_HOST', 'localhost')
+        app.config.setdefault('REDIS_PORT', 6379)
+        app.config.setdefault('REDIS_DB', 0)
+
+        app.before_request(self.before_request)
+
+        self.app = app
+
+    def connect(self):
+        return redis.Redis(host=self.app.config['REDIS_HOST'],
+                           port=self.app.config['REDIS_PORT'],
+                           db=self.app.config['REDIS_DB'])
+
+    def before_request(self):
+        g.redis = self.connect()
